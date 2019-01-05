94a7e4241a5765288556fc050f4d26d4a977e804
Administrator <admin@example.com>
2019-01-05 16:23:07 +0530

    SETUP SPRING BOOT APP

---
+++ src/main/java/org/hungrysnake/controllers/GameController.java
@@ -0,0 +1,15 @@
+package org.hungrysnake.controllers;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/api")
+public class GameController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test v1";
+    }
+}
